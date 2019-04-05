a2f8554e00fcc4c33c09d901e5a3e43b99d1dc00
root <root@user01-OptiPlex-7060>
2019-04-04 17:37:49 -0700

    Continue at home

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,38 @@
  *  Created on: Apr 3, 2019
  *      Author: root
  */
+#include <iostream>
+#include <functional>
 
+namespace Cat{
 
+template <typename T>
+T Id(T value){
+	return value;
+}
 
+template <typename Input, typename Middle, typename Output>
+std::function<Output(Middle)> Compose(std::function<Output(Middle)> first,std::function<Middle(Input)> second){
+	return [first, second](Input value)->Output{
+		return first(second(value));
+	};
+}
+
+}
+
+int main(int argc, char* argv[]){
+	using namespace Cat;
+
+	int c=0;
+	std::string cosa("this is my string");
+	std::cout<<Id(c)<<std::endl;
+	std::cout<<Id(cosa)<<std::endl;
+
+	auto square = [](int value)->int{return value*value;};
+
+	auto x = Compose(square,Id);
+	std::cout<<<<std::endl;
+
+	return 0;
+}
 
