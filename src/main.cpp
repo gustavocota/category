9a2ddcfd6ebb246c157c40b27bc37b8ca0d0d9ec
root <root@user01-OptiPlex-7060>
2019-04-08 12:02:19 -0700

    Finnaly can compose by using generic lambdas

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,11 @@
 
 namespace Cat{
 
-template<typename T>
-std::function<T(T)> IdCore = [](T value){return value;};
-//#define Id(value) IdCore<decltype(value)>(value)
-template<typename T>
-T Id(T value){
-	return IdCore<T>(value);
-}
+auto Id = [](auto value){return value;};
+auto Square = [](auto value){return value*value;};
+auto Compose = [](auto secondFunction, auto firstFunction){return [secondFunction,firstFunction](auto value){return secondFunction(firstFunction(value));};};
+auto Twice = [](auto value){return 2*value;};
 
-template<typename T>
-std::function<T(T)> SquareCore = [](T value){return value*value;};
-//#define Square(value) SquareCore<decltype(value)>(value)
-template<typename T>
-T Square(T value){
-	return SquareCore<T>(value);
-}
-
-template <typename A, typename B, typename C>
-std::function<C(A)> Compose(std::function<C(B)> g, std::function<B(A)> f)
-{
-  return [=](A a) -> C { return g(f(a)); };
-}
 }
 
 int main(int argc, char* argv[]){
@@ -41,10 +25,10 @@ int main(int argc, char* argv[]){
 	std::cout<<Id(c)<<std::endl;
 	std::cout<<Id(cosa)<<std::endl;
 	std::cout<<Square(.866025404)<<std::endl;
-	auto idSquare = Compose(IdCore<int>, SquareCore<int>);
-	auto squareId = Compose(SquareCore<int>, IdCore<int>);
-	std::cout<<idSquare(5)<<std::endl;
-	std::cout<<squareId(6)<<std::endl;
+	auto twiceOfSquare = Compose(Twice, Square);
+	auto squareOfTwice = Compose(Square, Twice);
+	std::cout<<twiceOfSquare(5)<<std::endl;
+	std::cout<<squareOfTwice(5)<<std::endl;
 
 
 	return 0;
